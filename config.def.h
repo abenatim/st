bb3c1d858d1b54306747c32e15c94bc12e88243a
aabm <aabm@disroot.org>
2021-03-16 18:38:50 -0300

    Make keybindings more emacs-y

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Iosevka Medium:pixelsize=15:antialias=true:autohint=true";
-static int borderpx = 2;
+static int borderpx = 12;
 
 /*
  * What program is execed by st depends of these precedence rules:
@@ -198,13 +198,15 @@ static Shortcut shortcuts[] = {
 	{ TERMMOD,              XK_Prior,       zoom,           {.f = +1} },
 	{ TERMMOD,              XK_Next,        zoom,           {.f = -1} },
 	{ TERMMOD,              XK_Home,        zoomreset,      {.f =  0} },
-	{ TERMMOD,              XK_C,           clipcopy,       {.i =  0} },
-	{ TERMMOD,              XK_V,           clippaste,      {.i =  0} },
+	{ Mod1Mask,             XK_w,           clipcopy,       {.i =  0} },
+	{ ControlMask,          XK_y,           clippaste,      {.i =  0} },
 	{ TERMMOD,              XK_Y,           selpaste,       {.i =  0} },
 	{ ShiftMask,            XK_Insert,      selpaste,       {.i =  0} },
 	{ TERMMOD,              XK_Num_Lock,    numlock,        {.i =  0} },
-	{ ShiftMask,            XK_Page_Up,     kscrollup,      {.i = -1} },
-	{ ShiftMask,            XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ TERMMOD,              XK_Page_Up,     kscrollup,      {.i = -1} },
+	{ TERMMOD,              XK_Page_Down,   kscrolldown,    {.i = -1} },
+	{ ControlMask,          XK_v,           kscrolldown,    {.i = -1} },
+	{ Mod1Mask,             XK_v,           kscrollup,      {.i = -1} },
 };
 
 /*
