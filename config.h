7e099d2fac1c3adce1179e395d70af4cf9b870e1
aabm <aabm@disroot.org>
2021-03-28 20:09:04 -0300

    Decrease font size to 11

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka medium extended:size=12:antialias=true:autohint=true";
+static char *font = "Iosevka medium extended:size=11:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
