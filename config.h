888e2a736378d1453d13733454669180f6d202fe
aabm <aabm@disroot.org>
2021-03-28 11:20:01 -0300

    Update font

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Iosevka Nerd Font:size=13:antialias=true:autohint=true";
+static char *font = "Iosevka medium extended:size=12:antialias=true:autohint=true";
 static int borderpx = 15;
 
 /*
